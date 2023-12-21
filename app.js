693c01f77b3ab31778f178e20c9f3ce7ec89a4a9
eliasyeme <eliasyeme@gmail.com>
2023-12-21 17:55:39 +0300

    Final check

--- app.js
+++ app.js
@@ -6,11 +6,22 @@ import Store from "./services/Store.js";
 import { MenuPage } from "./components/MenuPage.js";
 import { DetailsPage } from "./components/DetailsPage.js";
 import { OrderPage } from "./components/OrderPage.js";
+import ProductItem from "./components/ProductItem.js";
 
 window.app = {};
 app.store = Store;
+app.router = Router;
 
 window.addEventListener('DOMContentLoaded', () => {
     app.store.menu = loadData();
     Router.init()
+})
+
+window.addEventListener("appcartchange", event => {
+    const badge = document.getElementById("badge");
+    const qty = app.store.cart.reduce(
+        (acc, item) => acc + item.quantity, 0
+    );
+    badge.textContent = qty;
+    badge.hidden = qty == 0;
 })
